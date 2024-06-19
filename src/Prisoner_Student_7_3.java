f834d91f569f3281fc9e82199aa9073706e61e08
Aluno <Aluno@10.85.198.40>
2024-06-19 16:46:16 -0300

    Commit project PrisonerStudent7.3

---
+++ src/Prisoner_Student_7_3.java
@@ -0,0 +1,42 @@
+// Classe PrisonTest
+public class Prisoner_Student_7_3 {
+    public static void main(String[] args) {
+        // Instanciando um prisioneiro e definindo seus campos usando o método setFields()
+        Prisoner p02 = new Prisoner();
+        p02.setFields("Twitch", 1.73, 3);
+
+        Prisoner p01 = new Prisoner();
+        p01.setFields("Null", 0.00 , 0);
+
+        // Imprimindo as informações dos prisioneiros
+        System.out.println("Variavel: p02");
+        p02.printInfo();
+        System.out.println("\nVariavel: p01");
+        p01.printInfo();
+    }
+}
+
+// Classe Prisoner
+class Prisoner {
+    String name;
+    double height;
+    int sentence;
+
+    // Método para definir os valores dos campos
+    public void setFields(String name, double height, int sentence) {
+        this.name = name;
+        this.height = height;
+        this.sentence = sentence;
+    }
+
+    // Método para imprimir informações do prisioneiro
+    public void printInfo() {
+        System.out.println("Nome: " + name);
+        System.out.println("Altura: " + height + "m");
+        System.out.println("Sentenca: " + sentence + " anos");
+        
+        String prisoner = name + height + sentence;
+        System.out.println(prisoner);
+        
+    }
+}
